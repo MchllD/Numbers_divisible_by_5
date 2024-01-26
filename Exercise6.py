f7cdde15c294f0414b1856819b77cb60cdeee72a
MchllD <mich11dimafelix@gmail.com>
2024-01-26 10:07:57 +0800

    condition message

--- Exercise6.py
+++ Exercise6.py
@@ -18,6 +18,9 @@
 num_list = [11, 25, 30, 41, 55, 67, 75, 80]
 
 # condition message
+print("\nThere is a list of numbers. Let's identify the numbers that are divisible by 5.")
+print("\nGiven list:", num_list)
+
 # Create a list using list comprehension to store numbers divisible by 5
 # Print numbers divisible by 5
 # Iterate over the list of numbers divisible by 5
