08e882c54f17defb82e37407af974c873524fe6f
MchllD <mich11dimafelix@gmail.com>
2024-01-26 10:04:08 +0800

    given list of numbers

---
+++ Exercise6.py
@@ -0,0 +1,24 @@
+# Exercise 6: Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+
+# pseudocode
+
+
+# Given list of numbers
+# condition message
+# Create a list using list comprehension to store numbers divisible by 5
+# Print numbers divisible by 5
+# Iterate over the list of numbers divisible by 5
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ actual code ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+# Given list of numbers
+num_list = [11, 25, 30, 41, 55, 67, 75, 80]
+
+# condition message
+# Create a list using list comprehension to store numbers divisible by 5
+# Print numbers divisible by 5
+# Iterate over the list of numbers divisible by 5
+
